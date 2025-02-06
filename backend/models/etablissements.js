80040b9caaafda1ea3d8bcc52926497c61b43292
Othman Abdelmoumene <oabdelmoumene@gmail.com>
2025-02-06 23:54:31 +0100

    Modélisation des schémas de la BDD et ajouts des routes signin et signup pour les admins

---
+++ backend/models/etablissements.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const etablissementSchema = mongoose.Schema ({
+    name:String,
+    groupIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'groups' }]
+})
+
+const Etablissement = mongoose.model('etablissement', etablissementSchema);
+
+module.exports = Etablissement
--- backend/routes/admins.js
+++ backend/routes/admins.js
@@ -1,9 +1,62 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Admin = require("../models/admins");
+const uid2 = require("uid2");
+const { checkBody } = require("../modules/checkBody");
+
+// Route pour l'ajout d'un admin en BDD (signup)
+router.post("/signup", (req, res) => {
+  const signupFields = ["firstName", "lastName", "function", "role", "pictureUrl", "email", "password"];
+
+  // Vérification de la présence des données
+  if (!checkBody(req.body, signupFields)) {
+    res.json({ result: false, message: "Champs manquants ou vides" });
+  } else {
+    // Check si l'admin n'existe pas déjà via son email
+    Admin.findOne({email: req.body.email}).then((response) => {
+      if (response) {
+        res.json({ result: false, message: "Admin existe déjà en BDD"})
+      } else {
+        // Enregistrement de l'admin en BDD
+        const newAdmin = new Admin({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          function: req.body.function,
+          role: req.body.role,
+          pictureUrl: req.body.pictureUrl,
+          email: req.body.email,
+          password: req.body.password,
+          token: uid2(32),
+        });
+    
+        newAdmin.save().then((response) => {
+          res.json({ result: true, token: response.token });
+        });
+      }
+    });
+  }
+});
+
+
+// Route pour la connexion de l'admin (signin)
+router.post("/signin", (req, res) => {
+  const signinFields = ["email", "password"];
+
+  // Vérification de la présence des données
+  if (!checkBody(req.body, signinFields)) {
+    res.json({ result: false, message: "Champs manquants ou vides" });
+  } else {
+    Admin.findOne({email: req.body.email, password: req.body.password}).then((response) => {
+      if (!response) {
+        res.json({ result: false, message: "Aucun admin trouvé"})
+      } else {
+        res.json({ result: true, token: response.token })
+      }
+    })
+  }
+});
 
 /* GET admins listing. */
-router.get('/', function(req, res) {
-  
-});
+router.get("/", function (req, res) {});
 
 module.exports = router;
