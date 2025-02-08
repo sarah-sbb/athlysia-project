0388194aaf6da70a99aa471b9533e280e5cd5720
Sarah <24274796+sarah-sbb@users.noreply.github.com>
2025-02-07 18:53:02 -0800

    ajout du js réalisé en amont en js pure et réadaptation à react avec useState et useEffect

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,46 +1,160 @@
+import React, { useState, useEffect } from "react";
 import styles from '../styles/Navbar.module.css';
-//import Image from "next/image";
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';       <FontAwesomeIcon icon={faBookmark} />
-//import { faBookmark, faEyeSlash } from '@fortawesome/free-solid-svg-icons'; 
 
 function Navbar() {
+  const [activeAccordion, setActiveAccordion] = useState(null); // État actif pour les accordéons
+  const [activeSubItem, setActiveSubItem] = useState(""); // État actif pour les sous-menus
+
+  // Charger le sous-menu actif depuis localStorage au démarrage
+  useEffect(() => {
+    const savedActiveSubItem = localStorage.getItem("activeSubItem");
+    if (savedActiveSubItem) {
+      setActiveSubItem(savedActiveSubItem);
+    }
+  }, []);
+
+  // Fonction pour gérer les clics sur un élément de l'accordéon
+  const handleAccordionClick = (index) => {
+    // Si l'élément cliqué est déjà actif, on le referme
+    if (activeAccordion === index) {
+      setActiveAccordion(null);
+    } else {
+      setActiveAccordion(index); // Sinon, on l'ouvre
+    }
+  };
+
+  // Fonction pour gérer les clics sur un sous-élément
+  const handleSubItemClick = (href) => {
+    setActiveSubItem(href); // Mettre à jour l'état local
+    localStorage.setItem("activeSubItem", href); // Enregistrer dans localStorage
+  };
+
   return (
-    <nav classNameName={styles.mainNavbar}>
+    <nav className={styles.mainNavbar}>
       <div className={styles.accordionContainer}>
-          <div className={styles.accordionItem}>
-              <div className={styles.accordionHeader}>
-                  Tableau de bord
-              </div>
-          </div>
-          <div className={styles.accordionItem}>
-              <div className={styles.accordionHeader}>
-                  Groupes
-              </div>
-              <div className={styles.accordionContent}>
-                  <a href="/" className={styles.subItem}>Tous les groupes</a>
-                  <a href="/" className={styles.subItem}>Ajouter un groupe</a>
-              </div>
-          </div>
-      
-          <div className={styles.accordionItem}>
-              <div className={styles.accordionHeader}>Participants
-              </div>
-              <div className={styles.accordionContent}>
-                  <a href="/" className={styles.subItem}>Tous les participants</a>
-                  <a href="/" className={styles.subItem}>Ajouter un participant</a>
-              </div>
-          </div>
-      
-          <div className={styles.accordionItem}>
-              <div className={styles.accordionHeader}>
-                  Événements
-              </div>
-              <div className={styles.accordionContent}>
-                  <a href="/" className={styles.subItem}>Tous les événements</a>
-                  <a href="/" className={styles.subItem}>Créer un événement</a>
-              </div>
+        {/* Accordéon 1 */}
+        <div className={styles.accordionItem}>
+          <div
+            className={styles.accordionHeader}
+            onClick={() => handleAccordionClick(1)}
+          >
+            Tableau de bord
           </div>
         </div>
+
+        {/* Accordéon 2 */}
+        <div className={styles.accordionItem}>
+          <div
+            className={styles.accordionHeader}
+            onClick={() => handleAccordionClick(2)}
+          >
+            Groupes
+          </div>
+          <div
+            className={`${styles.accordionContent} ${
+              activeAccordion === 2 ? styles.active : ""
+            }`}
+            style={{
+              maxHeight: activeAccordion === 2 ? "200px" : "0", // Ajustement dynamique de la hauteur
+            }}
+          >
+            <a
+              href="/"
+              className={`${styles.subItem} ${
+                activeSubItem === "/" ? styles.activeSubItem : ""
+              }`}
+              onClick={() => handleSubItemClick("/")}
+            >
+              Tous les groupes
+            </a>
+            <a
+              href="/add-group"
+              className={`${styles.subItem} ${
+                activeSubItem === "/add-group" ? styles.activeSubItem : ""
+              }`}
+              onClick={() => handleSubItemClick("/add-group")}
+            >
+              Ajouter un groupe
+            </a>
+          </div>
+        </div>
+
+        {/* Accordéon 3 */}
+        <div className={styles.accordionItem}>
+          <div
+            className={styles.accordionHeader}
+            onClick={() => handleAccordionClick(3)}
+          >
+            Participants
+          </div>
+          <div
+            className={`${styles.accordionContent} ${
+              activeAccordion === 3 ? styles.active : ""
+            }`}
+            style={{
+              maxHeight: activeAccordion === 3 ? "200px" : "0",
+            }}
+          >
+            <a
+              href="/participants"
+              className={`${styles.subItem} ${
+                activeSubItem === "/participants" ? styles.activeSubItem : ""
+              }`}
+              onClick={() => handleSubItemClick("/participants")}
+            >
+              Tous les participants
+            </a>
+            <a
+              href="/add-participant"
+              className={`${styles.subItem} ${
+                activeSubItem === "/add-participant"
+                  ? styles.activeSubItem
+                  : ""
+              }`}
+              onClick={() => handleSubItemClick("/add-participant")}
+            >
+              Ajouter un participant
+            </a>
+          </div>
+        </div>
+
+        {/* Accordéon 4 */}
+        <div className={styles.accordionItem}>
+          <div
+            className={styles.accordionHeader}
+            onClick={() => handleAccordionClick(4)}
+          >
+            Événements
+          </div>
+          <div
+            className={`${styles.accordionContent} ${
+              activeAccordion === 4 ? styles.active : ""
+            }`}
+            style={{
+              maxHeight: activeAccordion === 4 ? "200px" : "0",
+            }}
+          >
+            <a
+              href="/events"
+              className={`${styles.subItem} ${
+                activeSubItem === "/events" ? styles.activeSubItem : ""
+              }`}
+              onClick={() => handleSubItemClick("/events")}
+            >
+              Tous les événements
+            </a>
+            <a
+              href="/add-event"
+              className={`${styles.subItem} ${
+                activeSubItem === "/add-event" ? styles.activeSubItem : ""
+              }`}
+              onClick={() => handleSubItemClick("/add-event")}
+            >
+              Créer un événement
+            </a>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
