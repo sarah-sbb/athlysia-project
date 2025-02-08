ac34b3c1d00b8332c15d595267fa386998496989
Sarah <24274796+sarah-sbb@users.noreply.github.com>
2025-02-07 18:35:56 -0800

    ajout du menu à la navbar et stylisation

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -5,9 +5,43 @@ import styles from '../styles/Navbar.module.css';
 
 function Navbar() {
   return (
-    <div>
-      <div className={styles.mainNavbar}>Navbar</div>
-    </div>  
+    <nav classNameName={styles.mainNavbar}>
+      <div className={styles.accordionContainer}>
+          <div className={styles.accordionItem}>
+              <div className={styles.accordionHeader}>
+                  Tableau de bord
+              </div>
+          </div>
+          <div className={styles.accordionItem}>
+              <div className={styles.accordionHeader}>
+                  Groupes
+              </div>
+              <div className={styles.accordionContent}>
+                  <a href="/" className={styles.subItem}>Tous les groupes</a>
+                  <a href="/" className={styles.subItem}>Ajouter un groupe</a>
+              </div>
+          </div>
+      
+          <div className={styles.accordionItem}>
+              <div className={styles.accordionHeader}>Participants
+              </div>
+              <div className={styles.accordionContent}>
+                  <a href="/" className={styles.subItem}>Tous les participants</a>
+                  <a href="/" className={styles.subItem}>Ajouter un participant</a>
+              </div>
+          </div>
+      
+          <div className={styles.accordionItem}>
+              <div className={styles.accordionHeader}>
+                  Événements
+              </div>
+              <div className={styles.accordionContent}>
+                  <a href="/" className={styles.subItem}>Tous les événements</a>
+                  <a href="/" className={styles.subItem}>Créer un événement</a>
+              </div>
+          </div>
+        </div>
+    </nav>
   );
 }
 
