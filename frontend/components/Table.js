73b626e43d3e3d0a37a22afe29d6a605c5ed42fc
Gilliane Arfaoui <gilliane.arfaoui@ecam.fr>
2025-02-09 18:49:43 +0100

    addition de Table

---
+++ frontend/components/Table.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import Paper from '@mui/material/Paper';
+
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+        field: 'fullName',
+        headerName: 'Full name',
+        description: 'This column has a value getter and is not sortable.',
+        sortable: false,
+        width: 160,
+        valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    },
+];
+
+const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon' },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei' },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime' },
+    { id: 4, lastName: 'Stark', firstName: 'Arya' },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys' },
+    { id: 6, lastName: 'Melisandre', firstName: null },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara' },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini' },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey' },
+];
+
+const paginationModel = { page: 0, pageSize: 5 };
+
+export default function DataTable() {
+    return (
+        <Paper sx={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+                sx={{ border: 0 }}
+            />
+        </Paper>
+    );
+}
+
+//overflow hidden : pour scroll dans un tableau sur css
