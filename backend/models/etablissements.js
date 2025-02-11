aef480cc72e8fbb9cb5706ca1cfbf7a88f9039f8
HassenSehawa <hs.benhaj@gmail.com>
2025-02-11 14:10:59 +0100

    collection admin : modification de la propriété etablissement en string

--- backend/models/etablissements.js
+++ backend/models/etablissements.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const etablissementSchema = mongoose.Schema ({
     name:String,
-    groupIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'groups' }]
+    // groupIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'groups' }]
 })
 
 const Etablissement = mongoose.model('etablissement', etablissementSchema);
