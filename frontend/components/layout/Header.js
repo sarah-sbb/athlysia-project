b25b1c5f8fcad60df38a84eaffdb97e4a0b96f06
Sarah <24274796+sarah-sbb@users.noreply.github.com>
2025-02-13 12:38:30 -0800

    ajout du titre dynamiquement en fonction des pages

--- frontend/components/layout/Header.js
+++ frontend/components/layout/Header.js
@@ -4,14 +4,14 @@ import Image from "next/image";
 //import { faBookmark, faEyeSlash } from '@fortawesome/free-solid-svg-icons'; 
 import Link from 'next/Link';
 
-function Header() {
+function Header({ title }) {
   return (
     <div className={styles.header}>
       <div className={styles.headerLogo}>
         <Image src="/iconeWhite.webp" alt="logo Athlysia" width={75} height={52}/>
       </div>
       <div className={styles.headerTitle}>
-        <h1>Titre de la page</h1>
+        <h1 className="hero_title">{title}</h1>
       </div>
       <div className={styles.headerNav}><Link href="/adminProfile"><a className={styles.link}>Mon profil</a></Link></div>
     </div>
