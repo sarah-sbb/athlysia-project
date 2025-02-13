835b65a3eed9f3a2c63130c9a79faf37b9436a7e
HassenSehawa <hs.benhaj@gmail.com>
2025-02-13 20:08:21 +0100

    suppression du groupID pour etablissement

--- backend/models/etablissements.js
+++ backend/models/etablissements.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 const etablissementSchema = mongoose.Schema ({
     name:String,
-    // groupIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'groups' }]
 })
 
 const Etablissement = mongoose.model('etablissement', etablissementSchema);
--- backend/routes/etablissements.js
+++ backend/routes/etablissements.js
@@ -5,7 +5,7 @@ const { checkBody } = require("../modules/checkBody");
 
 // Route pour la création d'un nouvel établissement
 router.post("/add", function (req, res) {
-  const fields = ["name", "groupIds"];
+  const fields = ["name"];
 
   // Vérification de la présence des données
   if (!checkBody(req.body, fields)) {
@@ -13,7 +13,6 @@ router.post("/add", function (req, res) {
   } else {
     const newEtablissement = new Etablissement({
       name: req.body.name,
-      groupIds: req.body.groupIds,
     });
 
     newEtablissement.save().then((response) => {
