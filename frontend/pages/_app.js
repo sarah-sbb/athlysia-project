56cc0c2e1b4364a22d655487dfcbead298e5c54c
HassenSehawa <hs.benhaj@gmail.com>
2025-02-13 19:50:49 +0100

    installation de reducer persist en cours

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,17 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
+//redux imports
+
+
+
+// redux-persist imports
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
+
+
 function App({ Component, pageProps }) {
   return (
     <>
