be401693daf28ef203af0a3be2cfc5727bcd16c6
Othman Abdelmoumene <oabdelmoumene@gmail.com>
2025-02-15 20:20:15 +0100

    Ajout d'une modal sur la page profil admin pour gérer les modifications de profil

--- frontend/components/ContentAdminProfile.js
+++ frontend/components/ContentAdminProfile.js
@@ -4,9 +4,23 @@ import Image from "next/image";
 //import { faBookmark, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import {
+  Modal,
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Select,
+  InputLabel,
+  MenuItem,
+  FormControl,
+} from "@mui/material";
 
 function Content() {
   const token = useSelector((state) => state.admin.value.token);
+
+  // Modal modifications des infos admin
+  const [open, setOpen] = useState(true);
 
   // Infos admin
   const [firstName, setFirstName] = useState("");
@@ -18,6 +32,40 @@ function Content() {
   // Infos groupes gérés par l'admin
   const [groupsData, setGroupsData] = useState([]);
   let groupsList = [];
+
+  const handleToggleModal = () => {
+    setOpen(!open);
+  };
+
+  const handleModify = () => {
+    console.log('test')
+  }
+
+  let modificationPopin = (
+    <Modal open={open} onClose={handleToggleModal}>
+      <Box sx={styleModal}>
+        {/* Formulaire */}
+        <Box sx={styleContainer}>
+          <TextField type="text" label="Prénom" name="firstName" />
+          <TextField type="text" label="Nom" name="lastName" />
+          <TextField type="text" label="Fonction" name="function" />
+          <TextField type="text" label="Email" name="email" />
+          <TextField type="text" label="URL photo" name="pictureUrl" />
+          <TextField type="password" label="Mot de passe" name="password" />
+        </Box>
+        {/* Footer avec les deux boutons */}
+
+        <Box sx={styleFooter}>
+          <Button sx={buttonCloseStyle} onClick={handleToggleModal}>
+            Fermer
+          </Button>
+          <Button sx={buttonSignUpStyle} onClick={handleModify}>
+            Valider
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
 
   useEffect(() => {
     // Récupération des infos relatives à l'admin lui-même
@@ -81,23 +129,95 @@ function Content() {
         />
       )}
       <ul>
-        <li className={styles.list}><strong>First name</strong> {firstName}</li>
-        <li className={styles.list}><strong>Last name</strong> {lastName}</li>
-        <li className={styles.list}><strong>Function</strong> {fonction}</li>
-        <li className={styles.list}><strong>Role</strong> {role}</li>
+        <li className={styles.list}>
+          <strong>First name</strong> {firstName}
+        </li>
+        <li className={styles.list}>
+          <strong>Last name</strong> {lastName}
+        </li>
+        <li className={styles.list}>
+          <strong>Function</strong> {fonction}
+        </li>
+        <li className={styles.list}>
+          <strong>Role</strong> {role}
+        </li>
       </ul>
-      <span>Modifier mon profil</span>
+      <a onClick={handleToggleModal}>Modifier mon profil</a>
+      {modificationPopin}
       <h3>Mes stats</h3>
       <h3>Mes groupes</h3>
-      <ul> 
-        {groupsData.length === 0 ? (
-          <span>Aucun groupe</span>
-        ) : groupsList}
+      <ul>
+        {groupsData.length === 0 ? <span>Aucun groupe</span> : groupsList}
       </ul>
       <h3>Mes sorties</h3>
       <h3>Mes autorisations</h3>
     </div>
   );
 }
+
+// MODAL STYLE
+//Composant MUI adapté au projet
+const styleModal = {
+  display: "grid",
+  gridTemplateRows: "auto 1fr auto",
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "background.paper",
+  borderRadius: 2,
+  p: 10,
+};
+
+const styleButtonOpenModal = {
+  bgcolor: "white",
+  fontSize: "0.75rem",
+  color: "#031EAD",
+};
+
+const styleHeader = {
+  textAlign: "center",
+  pb: 5,
+  fontSize: "1.1rem",
+};
+
+const styleContainer = {
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)",
+  gap: 2.5,
+  padding: 1,
+};
+
+const styleFooter = {
+  display: "flex",
+  justifyContent: "center",
+  pt: 5,
+  gap: 2,
+};
+
+const buttonCloseStyle = {
+  color: "#031EAD",
+  fontSize: "0.90rem",
+  width: 100,
+};
+const buttonSignUpStyle = {
+  bgcolor: "#031EAD",
+  color: "white",
+  fontSize: "0.90rem",
+  width: 100,
+};
+
+const styleErrorSignUp = {
+  display: "flex",
+  justifyContent: "center",
+  color: "red",
+};
+
+const styleSuccesSignUp = {
+  display: "flex",
+  justifyContent: "center",
+  color: "green",
+};
 
 export default Content;
