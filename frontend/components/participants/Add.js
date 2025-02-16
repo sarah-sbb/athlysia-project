34ea16ccbe91ecc4fbc643b27aa248ef56e3d9cd
Sarah <24274796+sarah-sbb@users.noreply.github.com>
2025-02-15 23:23:20 -0800

    amélioration de l'input

--- frontend/components/participants/Add.js
+++ frontend/components/participants/Add.js
@@ -1,18 +1,27 @@
 import styles from '../../styles/Participants.module.css';
 import { buttonStyles } from '../modules/Button';
-import Input from '../smallComponents/Input';
+import { Input } from '../modules/Input';
+
+//import Input from '../smallComponents/Input';
 
 function Add() {
   return (
     <div className={styles.participantContainer}>
-<div>Ajout d'un participant</div>
+        <div>Ajout d'un participant</div>
 
-<div className={styles.partButton}>
-  <button className={buttonStyles({ color: 'primary' })}>Title 1</button>
-  <button className={buttonStyles({ color: 'secondary' })}>Title 2</button>
-</div>
-
-<Input placeholder="my ass"/>
+        <div className={styles.partButton}>
+          <button className={buttonStyles(
+            { color: 'primary' })}>
+              Title 1
+          </button>
+          <button className={buttonStyles(
+            { color: 'secondary' })}>
+              Title 2
+          </button>
+        </div>
+        <div>
+          <Input label="Un input" />
+        </div>
     </div>
 
   );
