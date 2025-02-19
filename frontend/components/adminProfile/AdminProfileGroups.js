30b94e76559c9df483def8615e4d2c6a6e7d0c4b
Othman Abdelmoumene <oabdelmoumene@gmail.com>
2025-02-19 19:13:59 +0100

    Injection des data groupes sur page admin profil en dynamique via le sous composant

--- frontend/components/adminProfile/AdminProfileGroups.js
+++ frontend/components/adminProfile/AdminProfileGroups.js
@@ -16,11 +16,46 @@ import {
 } from "@mui/material";
 
 function AdminProfileGroups() {
+
+  // Récupération du token depuis redux
+  const token = useSelector((state) => state.admin.value.token);
+
+  // Infos groupes
+  const [groupsData, setGroupsData] = useState([]);
+  let groupsList = [];
+
+  // Récupération des infos relatives aux groupes gérés par l'admin
+  useEffect(() => {
+    fetch("http://localhost:3000/groups/findAllGroupsByAdminToken", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          setGroupsData(
+            data.data.map((element) => ({
+              title: element.title,
+              nbParticipants: element.participantIds.length,
+            }))
+          );
+        }
+      });
+  }, []);
+
+  // Transformation des données brutes des groupes pour affichage
+  groupsList = groupsData.map((e) => {
+    return (
+      <li className={styles.eventList}>
+        {e.title} - {e.nbParticipants} participant(s)
+      </li>
+    );
+  });
+
   return (
-    <div
-    style={{ border: "1px dashed", color: "blue", height: "200px", display: "flex", alignItems:"center", justifyContent: "center" }}
-    >
-      Les groupes seront ici
+    <div>
+      {groupsData.length === 0 ? <span>Aucun groupe</span> : groupsList}
     </div>
   );
 }
