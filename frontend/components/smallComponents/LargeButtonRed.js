ea5df8282589ac4018db93b2123294811244c26b
Sarah <24274796+sarah-sbb@users.noreply.github.com>
2025-02-19 22:11:31 -0800

    mise à jour des dossiers et fichiers

--- frontend/components/smallComponents/LargeButtonRed.js
+++
@@ -1,9 +0,0 @@
-import styles from "../../styles/smallComponents.module.css"
-
-function LargeButtonRed ({title}) {
-return (
-<button className={styles.largeButtonRed}>{title|| "Bouton Rouge"}</button>
-)
-}
-
-export default LargeButtonRed;
