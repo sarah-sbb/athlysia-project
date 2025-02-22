fdc5d57113292064af012f7b680156160c4de303
Sarah <24274796+sarah-sbb@users.noreply.github.com>
2025-02-21 19:59:25 -0800

    mise à jour de image.domains en images.remotePatterns

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',       // Protocole utilisé (Cloudinary utilise HTTPS)
+        hostname: 'res.cloudinary.com', // Domaine de Cloudinary
+        port: '',                // Laisse vide car Cloudinary ne requiert pas de port spécifique
+        pathname: '/**',         // Toutes les images sous ce domaine sont autorisées
+      },
+    ],
   },
 };
 
