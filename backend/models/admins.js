e8cf2d005bbfa1bc095a42153a55b3695b1dc927
Othman Abdelmoumene <oabdelmoumene@gmail.com>
2025-02-26 13:39:07 +0100

    Roll back sur la modif sur le model admins (pas de ENUM finalement)

--- backend/models/admins.js
+++ backend/models/admins.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose')
 
-const ROLES = [ "Admin", "Gestionnaire", "Viewer" ]; // la liste des rôles est définie ici
-
 const adminSchema = mongoose.Schema ({
     firstName:String,
     lastName:String,
     position:String,
-    // role: String,
-    role: [{type: String, enum: ROLES}],
+    role: String,
     pictureUrl:String,
     email: String,
     etablissement: { type: mongoose.Schema.Types.ObjectId, ref: 'etablissements' },
