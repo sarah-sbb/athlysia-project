91e579107702e2b541e9e02ddcec9bc678c1b788
Othman Abdelmoumene <oabdelmoumene@gmail.com>
2025-02-26 13:34:08 +0100

    Modification sur le model admins (ajouts des roles en ENUM)

--- backend/models/admins.js
+++ backend/models/admins.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
+const ROLES = [ "Admin", "Gestionnaire", "Viewer" ]; // la liste des rôles est définie ici
+
 const adminSchema = mongoose.Schema ({
     firstName:String,
     lastName:String,
     position:String,
-    role: String,
+    // role: String,
+    role: [{type: String, enum: ROLES}],
     pictureUrl:String,
     email: String,
     etablissement: { type: mongoose.Schema.Types.ObjectId, ref: 'etablissements' },
